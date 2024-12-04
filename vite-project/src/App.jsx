80b967078d3bfc47d7ce661a9f0756e349a604a6
Niresh <niresh1606@gmail.com>
2024-12-04 20:49:42 +0530

    learning: learned how to chage the code and created the componet and included

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,15 @@
+import { LearnComponents } from "./component/LearnComponents";
 
 
 function App() {
 
 
-  return <div>This is the project on react</div>
-    
-
-  
+  return (
+  <div>
+  <>This is the project on react , used the fragment tag, without any additional div</>
+    <LearnComponents/>
+  </div>
+  );
 }
 
 export default App
